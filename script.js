4e6a6eaf1bcf18af27a931f5e0cdfd9b2da2829e
Michal Nebeský <77640963+NebeskyMichal@users.noreply.github.com>
2022-09-07 13:42:07 +0200

    Verze 0.2

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
+    const uluru = { lat: 48.6940846, lng: 21.2237353 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
       center: uluru,
     });
-    // The marker, positioned at Uluru
+
+    google.maps.event.addListener(map, 'click', function (e) {
+ 
+      var location = e.latLng;
+
+      var marker = new google.maps.Marker({
+          position: location,
+          map: map
+      });
+
+      google.maps.event.addListener(marker, "click", function (e) {
+          var infoWindow = new google.maps.InfoWindow({
+              content: 'Latitude: ' + location.lat() + '<br />Longitude: ' + location.lng()
+          });
+          infoWindow.open(map, marker);
+      });
+    });
+
     const marker = new google.maps.Marker({
       position: uluru,
       map: map,
